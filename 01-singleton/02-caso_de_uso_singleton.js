98fc381bdf7c36c0b82601ff2abd949c98d88b0a
Octavillt <ovillafrancot@gmail.com>
2024-02-02 01:25:51 -0600

    Se agrega documentación para el patrón de diseño Singleton para un mejor entendimiento del código

--- 01-singleton/02-caso_de_uso_singleton.js
+++ 01-singleton/02-caso_de_uso_singleton.js
@@ -1,11 +1,18 @@
 // Implementación del Patrón Singleton en un Caso de Uso Práctico
-
+/**
+ * Clase WeekDays para manejar nombres de días de la semana en diferentes idiomas.
+ * Implementa el patrón Singleton para asegurar una única instancia de la clase.
+ */
 class WeekDays {
     // Arrays que contienen los nombres de los días de la semana en español e inglés.
     daysEs = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
     daysEn = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
-    // Constructor de la clase WeekDays que acepta un parámetro 'lang' para el idioma.
+    /**
+     * Constructor de la clase WeekDays.
+     * Asegura la creación de una sola instancia de la clase.
+     * @param {string} lang - El idioma para los días de la semana ('es' para español, 'en' para inglés).
+     */
     constructor(lang) {
         this.lang = lang; // Asigna el idioma especificado a la propiedad 'lang'.
 
@@ -20,7 +27,10 @@ class WeekDays {
         // Esto asegura que todas las futuras instancias de WeekDays sean la misma que la primera creada.
     }
 
-    // Método getDays para obtener los días de la semana según el idioma establecido.
+    /**
+     * Método para obtener los días de la semana según el idioma establecido.
+     * @returns {string[]} Los días de la semana en el idioma seleccionado.
+     */
     getDays() {
         // Retorna los días de la semana en el idioma seleccionado.
         return this.lang === "es" ? this.daysEs : this.daysEn;
